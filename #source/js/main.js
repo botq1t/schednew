7b0104609174dc9cd70df25f054c4a3cab55d827
botq1t <77788765+botq1t@users.noreply.github.com>
2021-10-07 17:20:22 +0300

    colorschemes

--- #source/js/main.js
+++ #source/js/main.js
@@ -6,3 +6,15 @@ $('.nav-v__tab').click(function () {
 	});
 	$(this).addClass('active');
 });
+
+$('.nav-h__tab').click(function () {
+	$('.nav-h__tab').each(function () {
+		$(this).removeClass('active');
+	});
+	$(this).addClass('active');
+});
+
+$('.info__toggler').click(function () {
+	$('.info__toggler').toggleClass('active');
+	$('.info').toggleClass('active');
+});
