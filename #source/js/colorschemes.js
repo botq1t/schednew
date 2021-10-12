8db726720c5dbfd62bf5cc914b9f9028ebea9ac2
botq1t <77788765+botq1t@users.noreply.github.com>
2021-10-12 21:30:17 +0300

    bug-fix

--- #source/js/colorschemes.js
+++ #source/js/colorschemes.js
@@ -10,7 +10,12 @@ let defaultSettings = {
 if (!localStorage['schednew_settings']) localStorage['schednew_settings'] = JSON.stringify(defaultSettings);
 
 
+
 let settings = JSON.parse(localStorage['schednew_settings']);
+if (!settings.group) {
+	localStorage.removeItem('schednew_settings');
+	document.location.reload();
+}
 console.log(settings);
 
 $('head').append(`<link rel="stylesheet" href="css/colorschemes/${settings.colorScheme}.css">`);
